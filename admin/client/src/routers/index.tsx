035b1be8ed663d2c0cb91a000769909aae0d8a86
hoang pham <phamminhhoangf3@gmail.com>
2024-09-17 11:29:54 +0700

    code splitting page admin

--- admin/client/src/routers/index.tsx
+++ admin/client/src/routers/index.tsx
@@ -1,16 +1,18 @@
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import Users from "~/pages/users";
 import { paths } from "~/constants/path";
 import Login from "~/pages/auth/login";
 import MainLayout from "~/layout/main-layout";
 import { useAuth } from "~/authProvider";
 import AuthLayout from "~/layout/auth";
-import DetailUser from "~/pages/users/detail";
-import Members from "~/pages/members";
-import DetailMember from "~/pages/members/detail";
-import Family from "~/pages/family";
-import DetailFamily from "~/pages/family/detail";
-import Home from "~/pages/home";
+import React from "react";
+
+const Home = React.lazy(() => import("~/pages/home"));
+const Members = React.lazy(() => import("~/pages/members"));
+const Users = React.lazy(() => import("~/pages/users"));
+const Family = React.lazy(() => import("~/pages/family"));
+const DetailFamily = React.lazy(() => import("~/pages/family/detail"));
+const DetailMember = React.lazy(() => import("~/pages/members/detail"));
+const DetailUser = React.lazy(() => import("~/pages/users/detail"));
 
 const RejectedRoute = () => {
   const { isAuthenticated } = useAuth();
@@ -35,38 +37,49 @@ const Root = () => {
 
 export default function RouteElements() {
   return (
-    <Routes>
-      <Route path="/" element={<Root />} />
-      <Route path="/auth" element={<RejectedRoute />}>
-        <Route path="login" element={<Login />} />
-      </Route>
-      <Route path="/admin" element={<MainLayout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="user">
-          <Route index element={<Users />} />
-          <Route path="add" element={<DetailUser typePage="add" />} />
-          <Route path="view/:id" element={<DetailUser typePage="detail" />} />
-          <Route path="update/:id" element={<DetailUser typePage="update" />} />
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/auth" element={<RejectedRoute />}>
+          <Route path="login" element={<Login />} />
         </Route>
-        <Route path="members">
-          <Route index element={<Members />} />
-          <Route path="add" element={<DetailMember typePage="add" />} />
-          <Route path="view/:id" element={<DetailMember typePage="detail" />} />
-          <Route
-            path="update/:id"
-            element={<DetailMember typePage="update" />}
-          />
+        <Route path="/admin" element={<MainLayout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="user">
+            <Route index element={<Users />} />
+            <Route path="add" element={<DetailUser typePage="add" />} />
+            <Route path="view/:id" element={<DetailUser typePage="detail" />} />
+            <Route
+              path="update/:id"
+              element={<DetailUser typePage="update" />}
+            />
+          </Route>
+          <Route path="members">
+            <Route index element={<Members />} />
+            <Route path="add" element={<DetailMember typePage="add" />} />
+            <Route
+              path="view/:id"
+              element={<DetailMember typePage="detail" />}
+            />
+            <Route
+              path="update/:id"
+              element={<DetailMember typePage="update" />}
+            />
+          </Route>
+          <Route path="family">
+            <Route index element={<Family />} />
+            <Route path="add" element={<DetailFamily typePage="add" />} />
+            <Route
+              path="view/:id"
+              element={<DetailFamily typePage="detail" />}
+            />
+            <Route
+              path="update/:id"
+              element={<DetailFamily typePage="update" />}
+            />
+          </Route>
         </Route>
-        <Route path="family">
-          <Route index element={<Family />} />
-          <Route path="add" element={<DetailFamily typePage="add" />} />
-          <Route path="view/:id" element={<DetailFamily typePage="detail" />} />
-          <Route
-            path="update/:id"
-            element={<DetailFamily typePage="update" />}
-          />
-        </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </React.Suspense>
   );
 }
